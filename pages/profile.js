7e4a1b99d33b8deed11c38b1c7f58c6cb83d18f9
Haseeb Javed <hjaved@egenienext.com>
2020-10-29 19:16:57 +0500

    auth middleware api setup

---
+++ pages/profile.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { withAuthSync, getUserData } from '../utils/auth';
+import { logout } from '../utils/auth'
+import axios from 'axios';
+
+import { Button, Menu, Dropdown } from 'antd';
+
+const ProfilePage = () => {
+
+    const [getUser, setGetUser] = useState({});
+    const handleLogout = (event) => {
+        axios
+          .delete('/api/session')
+          .then(({data}) => {
+              console.log('data for logout :',data)
+            if (data.status === 'ok') {
+              logout();
+            }
+          });
+      };
+
+    useEffect(() => {
+        setGetUser(getUserData());
+    }, [])
+
+
+    return (
+        <>
+        <Button type="primary" onClick={handleLogout}>logout</Button>
+            <h1>Profile</h1>
+            <div>
+                <p>
+                    Name:
+  {' '}
+                    {getUser.name}
+                </p>
+                <p>
+                    Email:
+  {' '}
+                    {getUser.email}
+                </p>
+
+            </div>
+        </>
+    )
+}
+
+export default withAuthSync(ProfilePage);
